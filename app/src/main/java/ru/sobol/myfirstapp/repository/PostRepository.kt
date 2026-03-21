2baf542363f6f4e8fc76dade3ddb5b0cd7817405
KING1c <157579531+KING1c@users.noreply.github.com>
2026-03-21 10:44:43 +0300

    Day 6: Added RecyclerView with posts ist.

--- app/src/main/java/ru/sobol/myfirstapp/repository/PostRepository.kt
+++ app/src/main/java/ru/sobol/myfirstapp/repository/PostRepository.kt
@@ -1,18 +1,11 @@
 package ru.sobol.myfirstapp.repository
 
-    import androidx.lifecycle.LiveData
-    import ru.sobol.myfirstapp.dto.Post
+import androidx.lifecycle.LiveData
+import ru.sobol.myfirstapp.dto.Post
 
-    interface PostRepository {
-        // Возвращает LiveData, на которую можно подписаться
-        fun get(): LiveData<Post>
-
-        // Лайк/дизлайк
-        fun like()
-
-        // Репост (увеличение счетчика)
-        fun share()
-
-        // Изменение просмотров (может пригодиться позже)
-        fun increaseViews()
-    }
+interface PostRepository {
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Long)
+    fun shareById(id: Long)
+    fun increaseViews(id: Long)
+}
